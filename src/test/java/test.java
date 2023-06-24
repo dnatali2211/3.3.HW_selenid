47c0c466ac762c72a01ba080a35b86b92ed42e61
Nataliia Bulatovskaia <dnatali2211@gmail.com>
2023-06-24 18:07:26 +0300

    Improved code

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -40,14 +40,12 @@ public class test {
     @Test
     void shouldTest() throws InterruptedException {
         List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Иванова Мария");
-        elements.get(1).sendKeys("+79317654934");
+        elements.get(0).sendKeys("Иванова Мария-Анна");
+        elements.get(1).sendKeys("+71286493096");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
-        assertEquals("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text);
-
-        Thread.sleep(5000);
+        String text = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 }
 

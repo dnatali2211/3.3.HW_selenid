23e48b6242322af88c7c3375f8365a759c6607e9
Nataliia Bulatovskaia <dnatali2211@gmail.com>
2023-06-25 20:53:25 +0300

    Added recommended changes

--- src/test/java/TestAlfaCard.java
+++ src/test/java/TestAlfaCard.java
@@ -1,19 +1,16 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class test {
+public class TestAlfaCard {
     private WebDriver driver;
 
     @BeforeAll
@@ -28,7 +25,7 @@ public class test {
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
-        driver.get("http://0.0.0.0:9999");
+        driver.get("http://localhost:9999");
     }
 
     @AfterEach
@@ -38,13 +35,12 @@ public class test {
     }
 
     @Test
-    void shouldTest() throws InterruptedException {
-        List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Иванова Мария-Анна");
-        elements.get(1).sendKeys("+71286493096");
+    void shouldTest() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иванова Мария-Анна");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+71286493096");
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
+        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 }
